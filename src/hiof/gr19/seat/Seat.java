c65368d8406af699300f241d357f96ea37998447
TobiasTR <tobiast98kontakt@protonmail.com>
2019-03-12 15:27:23 +0100

    metode for å få type bruker :ok_hand:

--- src/hiof/gr19/seat/Seat.java
+++ src/hiof/gr19/seat/Seat.java
@@ -1,9 +1,37 @@
 package hiof.gr19.seat;
 
+import java.util.Scanner;
+
 public class Seat {
     public static void main(String[] args){
-        System.out.println("ok");
-        
 
+        identifyUser();
+
+    }
+
+    private static void identifyUser() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("OP mode?");
+        System.out.println("1 = Organizer");
+        System.out.println("2 = Not Organizer");
+        System.out.print(">");
+        int systemMode = input.nextInt();
+
+        switch (systemMode){
+            case 1:
+                System.out.println("organizer");
+                //organizerMode()
+                break;
+
+            case 2:
+                System.out.println("Customer");
+                break;
+
+            default:
+                System.out.println("Please choose 1 or 2 :)");
+                main(null);
+                break;
+        }
     }
 }
