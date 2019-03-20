d6281037077de9dab4a2d5027d7014c39b603bfd
EmHaB <emhabahe@hotmail.com>
2019-03-20 01:30:47 +0100

    Lagt til DB delete event og UserRemoveTicket Endret Organizer id fra String til int .gitignore ignorerer lib/

--- src/hiof/gr19/seat/Seat.java
+++ src/hiof/gr19/seat/Seat.java
@@ -8,7 +8,9 @@ public class Seat {
     private static Database test = new Database();
 
     public static void main(String[] args){
-        identifyUser();
+        //identifyUser();
+
+        // Denne funksjonen inneholder get, insert og delete eksempler for å sjekke om de funker
         tempFunctionToTestDatabaseConnectionWorks();
     }
 
@@ -50,6 +52,33 @@ public class Seat {
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        try {
+            // Arrangøren her er et dummy eksempel og ikke en Organizer hentet fra databasen
+            Arrangement exampleArr = new Arrangement(null, "Fyre Festival","no scam come",new Date(2019,5,1),new Organizer(1,"organiiiiizeyy"),50);
+            test.createEvent(exampleArr);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ResultSet resultSet = test.displayEvents();
+            System.out.println("\nEvents in our system:");
+
+            while(resultSet.next())
+                System.out.println("-\t" + resultSet.getString("navn") + " " + resultSet.getString("beskrivelse") + " " + resultSet.getString("dato") + " " + resultSet.getInt("arrangor_fk"));
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            test.cancelEvent(1);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("An event should be deleted from the system.");
 
         try {
             ResultSet resultSet = test.displayEvents();
