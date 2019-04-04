067c14a58445314e833300162a8c158cfae9cc22
TobiasTR <tobiast98kontakt@protonmail.com>
2019-04-04 16:34:29 +0200

    splittet ConsoleInterface til en pakke med 2 klasser 1 for kunde og 1 for arrangør. :pray:

---
+++ src/hiof/gr19/seat/console/ui/CustomerConsole.java
@@ -0,0 +1,50 @@
+package hiof.gr19.seat.console.ui;
+
+import hiof.gr19.seat.Arrangement;
+
+import java.io.IOException;
+
+public class CustomerConsole extends Console{
+
+    static void customerMenu(){
+
+        java.io.Console console = System.console();
+        if (console == null){
+            throw new NullPointerException("No console found");
+        }
+
+        System.out.println("Choose arrangement based on ID");
+        //TODO print all arrangments
+
+        String arrangmentID = console.readLine(">");
+        //TODO metode for å hente arrangement basert på id
+
+
+        try {
+            purchaseTicketMenu(null);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+    static void purchaseTicketMenu(Arrangement arrangement) throws IOException {
+        java.io.Console console = System.console();
+        if (console == null){
+            throw new NullPointerException("No console found");
+        }
+
+        System.out.println("How many tickets?");
+        String ticketAmount = console.readLine(">");
+
+
+        if (Integer.parseInt(ticketAmount) > arrangement.getPeopleAmount()){
+            throw new IOException("No more tickets left");
+
+        }
+
+    }
+
+}
