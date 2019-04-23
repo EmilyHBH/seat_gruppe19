86ea1133ca0bf7c49bd57ef978ba27f225d88b17
EmilyHBH <emhabahe@hotmail.com>
2019-04-23 18:39:01 +0200

    test registrer ny arrangør generaterandomstring funksjon for å bruke for testing laget i ConsoleTest

--- test/hiof/gr19/seat/console/ui/OrganizerConsoleTest.java
+++ test/hiof/gr19/seat/console/ui/OrganizerConsoleTest.java
@@ -1,8 +1,13 @@
 package hiof.gr19.seat.console.ui;
 
+import hiof.gr19.seat.Database;
 import hiof.gr19.seat.model.Organizer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.Charset;
+import java.sql.SQLException;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +31,26 @@ class OrganizerConsoleTest extends ConsoleTest{
 		Organizer organizer = oc.organizerLogin();
 
 		assertEquals("OrgStarter",organizer.getOrganizerName());
+
+	}
+
+	@Test
+	public void registrerNyArrangor(){
+
+		String testNavn = buildRandomString(15);
+		String madeUpEmail = "email@email.email";
+
+		provideInput(testNavn+ENTER+madeUpEmail+ENTER);
+
+		oc.registerOrganizer();
+
+		Database db = new Database();
+
+		try {
+			assertEquals(testNavn, db.getOrganizerByName(testNavn).getOrganizerName());
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
